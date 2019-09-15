1e1ea2dda0629612a0076ea30e1b5543f6c41302
Ankit Agarwal <ankit.agarwal@metacube.com>
2019-09-15 20:19:38 +0530

    Added API Connector to delete and query records,introduction of BasePage class,update TSMCRequestPage class

--- src/main/java/pageObjects/TSMCRequestPage.java
+++ src/main/java/pageObjects/TSMCRequestPage.java
@@ -1,27 +1,22 @@
 package pageObjects;
 
-import helpers.SFHelper;
-import junit.framework.Assert;
-
-import static org.junit.Assert.assertEquals;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
-public class TSMCRequestPage {
-	WebDriver driver;
+public class TSMCRequestPage extends BasePage{
+	
 	public TSMCRequestPage(WebDriver driver){
-		this.driver = driver;
+		super(driver);
 		PageFactory.initElements(driver, this);
 	}
 	
 	//*********************** Generic Xpaths ***************
 	public static String  error_message_below_fields = "//*[text()='%s']/ancestor::td[1]/following-sibling::td[1]//div[text()=' You must enter a value']";
-	
+	public String  field_value_xpath = "//td[text()='%s']/following-sibling::td[1]/div";
+
 	//**********Buttons Selector*************//
 	@FindBy(how = How.CSS, using = "div.filterOverview input[name=go]")
 	private WebElement button_go;
@@ -41,16 +36,51 @@ public class TSMCRequestPage {
 	@FindBy(how = How.NAME, using = "edit")
 	private WebElement button_edit;
 	
+	@FindBy(how = How.NAME, using = "submit")
+	private WebElement button_submitForApproval;
 	
-	//**********Fields Selector*************//
+	//**********Edit Page Fields Selector*************//
 	@FindBy(how = How.ID, using = "fcf")
-	private WebElement picklist_opportunityView;
+	private WebElement picklist_tsmcView;
 	
 	@FindBy(how = How.XPATH, using = "//span[text()='T' and @class='listItemPad']")
 	private WebElement list_view_quick_icon;
 	
-	//@FindBy(how = How.XPATH, using = String.format(error_message_below_fields, 'adf'))
-	//private WebElement error_message_for_fields;
+	@FindBy(how = How.XPATH, using = "//*[text()='Purpose']/../following-sibling::td/div/span/select")
+	private WebElement picklist_purpose;
+	
+	@FindBy(how = How.XPATH, using = "//*[text()='Process Node']/../following-sibling::td/div//select")
+	private WebElement picklist_processNode;
+	
+	@FindBy(how = How.XPATH, using = "//*[text()='Employee Type']/../following-sibling::td/div/span/select")
+	private WebElement picklist_employeeType;
+	
+	@FindBy(how = How.XPATH, using = "//input[@title='User']")
+	private WebElement textbox_user;
+	
+	@FindBy(how = How.XPATH, using = "//*[text()='EDA Tool,IP Name,DS project and customer']/../../following-sibling::td/div//input")
+	private WebElement textbox_edaRequest;
+	
+	@FindBy(how = How.XPATH, using = "//*[text()='Support Type']/../following-sibling::td/div//select")
+	private WebElement picklist_supportType;
+	
+	@FindBy(how = How.XPATH, using = "//*[text()='Permitted Purpose']/../../following-sibling::td/div//input")
+	private WebElement textbox_permittedPurpose;
+	
+	@FindBy(how = How.XPATH, using = "//*[text()='Field(Sales/AE)']/../../following-sibling::td/span/select")
+	private WebElement picklist_fieldSalesAE;
+	
+	@FindBy(how = How.XPATH, using = "//*[text()='Physical Location']/../following-sibling::td/div//select")
+	private WebElement picklist_physicalLocation;
+	
+	@FindBy(how = How.XPATH, using = "//*[text()='Detailed Job Description']/../../following-sibling::td/div/textarea")
+	private WebElement textarea_detailedJobDescription;
+	
+	@FindBy(how = How.XPATH, using = "//*[text()='Remark']/../following-sibling::td/textarea")
+	private WebElement textarea_remark;
+	
+	
+	//***********Error Message Selectors*******//
 	
 	@FindBy(how = How.XPATH, using = "//*[text()='Purpose']/ancestor::td[1]/following-sibling::td[1]//div[text()=' You must enter a value']")
 	private WebElement error_message_for_purpose_field;
@@ -76,88 +106,117 @@ public class TSMCRequestPage {
 	@FindBy(how = How.XPATH, using = "//*[text()='Support Type']/ancestor::td[1]/following-sibling::td[1]//div[text()=' You must enter a value']")
 	private WebElement error_message_for_support_type_field;
 
-	//    	//label[text()='Purpose']/ancestor::td[1]/following-sibling::td[1]//select
-	//		//*[text()='EDA Tool,IP Name,DS project and customer']/ancestor::td[1]/following-sibling::td[1]//input
-
+	
 	//**********Methods *******************//
 	
 	public void clickListViewQuickIcon(){
-		list_view_quick_icon.click();
+		click(list_view_quick_icon);
 	}
 	
 	public void clickContinueButton(){
-		button_continue.click();
+		click(button_continue);
 	}
 	
 	public void clickSaveButton(){
-		button_save.click();
+		click(button_save);
 	}
 	
 	public void clickEditButton(){
-		button_edit.click();
+		click(button_edit);
 	}
 	
 	public void clickNewButton(){
-		button_new.click();
+		click(button_new);
 	}
 
 	public void clickNewTSMCRequest()
 	{
-		button_new_tsmc_request.click();
+		click(button_new_tsmc_request);
 	}
 	
 	public void clickGoButton(){
-		button_go.click();
-	}
-
-	public void clickOpportunityOnListView(String opportunity_name) {
-		SFHelper.click_link(driver, opportunity_name);
-	}
-	
-	public void clickEditOpportunityOnListView(String opportunity_name) {
-		SFHelper.click_Edit_link_on_list_view(driver, opportunity_name);
+		click(button_go);
 	}
 	
 	public void selectPurpose(String purpose) {
-		selectPicklist(picklist_opportunityView, purpose);
+		selectPicklist(picklist_purpose, purpose);
 	}
-
-	//*****************Need to move this Method************************//
-	private void selectPicklist(WebElement selector, String type){
-		Select picklist = new Select(selector);
-		picklist.selectByValue(type);
+	
+	public void selectProcessNode(String process_node) {
+		selectPicklist(picklist_processNode, process_node);
 	}
-
+	
+	public void selectEmployeeType(String employee_type) {
+		selectPicklist(picklist_employeeType, employee_type);
+	}
+	
+	public void selectSupportType(String support_type) {
+		selectPicklist(picklist_supportType, support_type);
+	}
+	
+	public void selectPhysicalLocation(String physical_location) {
+		selectPicklist(picklist_physicalLocation, physical_location);
+	}
+	
+	public void selectField(String field) {
+		selectPicklist(picklist_fieldSalesAE, field);
+	}
+	
+	public void setUser(String user) {
+		type(textbox_user,user);
+	}
+	
+	public void setEDARequest(String eda_request) {
+		type(textbox_edaRequest,eda_request);
+	}
+	
+	public void setPermittedPurpose(String permitted_purpose) {
+		type(textbox_permittedPurpose,permitted_purpose);
+	}
+	
+	public void setJobDescription(String job_description) {
+		type(textarea_detailedJobDescription,job_description);
+	}
+	
+	public void setRemark(String remark) {
+		type(textarea_remark,remark);
+	}
+	
+	public String getTSMCFieldValue(String fieldName) {
+		String selector = String.format(field_value_xpath, fieldName);
+		return getTextByXpath(selector);
+	}
+	
 	public void assertErrorMessageOnPurposeField() {
-		assertEquals("Error: You must enter a value", error_message_for_purpose_field.getText());
+		assertValue("Error: You must enter a value", error_message_for_purpose_field.getText());
 	}
 
 	public void assertErrorMessageOnEmployeeTypeField() {
-		assertEquals("Error: You must enter a value", error_message_for_employee_type_field.getText());
+		assertValue("Error: You must enter a value", error_message_for_employee_type_field.getText());
 		
 	}
 
 	public void assertErrorMessageOnEdaToolIPNameDSProjectandCustomerField() {
-		assertEquals("Error: You must enter a value", error_message_for_eda_tool_ip_name_field.getText());
+		assertValue("Error: You must enter a value", error_message_for_eda_tool_ip_name_field.getText());
 	}
 	
 	public void assertErrorMessageOnPermittedPurposeField() {
-		assertEquals("Error: You must enter a value", error_message_for_permitted_purpose_field.getText());
+		assertValue("Error: You must enter a value", error_message_for_permitted_purpose_field.getText());
 	}
 	
 	public void assertErrorMessageOnPhysicalLocationField() {
-		assertEquals("Error: You must enter a value", error_message_for_physical_location_field.getText());
+		assertValue("Error: You must enter a value", error_message_for_physical_location_field.getText());
 	}
 
 	public void assertErrorMessageOnDetailedJobDescriptionField() {
-		assertEquals("Error: You must enter a value", error_message_for_detailed_job_description_field.getText());
+		assertValue("Error: You must enter a value", error_message_for_detailed_job_description_field.getText());
 	}
 
 	public void assertErrorMessageOnUserField() {
-		assertEquals("Error: You must enter a value", error_message_for_user_field.getText());
+		assertValue("Error: You must enter a value", error_message_for_user_field.getText());
 	}
 
 	public void assertErrorMessageOnSupportTypeField() {
-		assertEquals("Error: You must enter a value", error_message_for_support_type_field.getText());
+		assertValue("Error: You must enter a value", error_message_for_support_type_field.getText());
 	}
 }
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -2,12 +2,10 @@ package stepDefinitions;
 
 import java.io.IOException;
 
-import managers.WebDriverHandler;
-
 import com.sforce.async.AsyncApiException;
 import com.sforce.ws.ConnectionException;
 
-import integrationHelpers.SFIntegrator;
+import integrationHelpers.SFBulkIntegrator;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
@@ -15,7 +13,7 @@ public class Hooks {
 	
 	@Before
 	public void beforeScenario() throws AsyncApiException, ConnectionException, IOException{
-		SFIntegrator integrator = new SFIntegrator();
+		SFBulkIntegrator integrator = new SFBulkIntegrator();
 		integrator.createRecords("Opportunity", "testusertest@test.com", "welcome@1GXcj25SJUCPsJozxbdLRwz2J", "Data/Opportunity.csv");
 	}
 	
