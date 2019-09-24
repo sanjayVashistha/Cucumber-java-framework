44061e3e155fcd5fd0e29ef479ee6e799529ea06
sanjayVashistha <44004243+sanjayVashistha@users.noreply.github.com>
2019-09-24 13:24:29 +0530

    Added getEchoSignLink method

--- src/main/java/integrationHelpers/GmailIntegrator.java
+++ src/main/java/integrationHelpers/GmailIntegrator.java
@@ -27,6 +27,7 @@ import javax.activation.FileDataSource;
 import javax.mail.Address;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
+import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
@@ -109,6 +110,18 @@ public class GmailIntegrator {
     	Message msg = listMessagesMatchingQuery(service, user, threadSubject);
     	return msg.getPayload().getParts().get(1).getBody().getData().toString();
     }
+    
+    public static String getEchoSignLink(String tsmcRecordName) throws IOException, MessagingException, GeneralSecurityException{
+    	String emailSubject = "Please sign "+tsmcRecordName+" Acknowledgement Letter";
+    	Gmail service = getService();
+    	String user = "Me";
+    	Message msg = listMessagesMatchingQuery(service, user, emailSubject);
+    	MimeMessage mimMsg = getMimeMessage(service,user,msg.getId());
+    	String msgText = getText(mimMsg);
+    	String echoSignLink = msgText.substring(msgText.indexOf("href=\"")+6);
+    	echoSignLink = echoSignLink.substring(0, echoSignLink.indexOf("\""));
+    	return echoSignLink;
+    }
 
     private static List<Thread> listThreadsMatchingQuery(Gmail service, String userId, String query) throws IOException {
     	
@@ -151,6 +164,9 @@ public class GmailIntegrator {
 	    		
 	    }
 	    Message msg = getLatestMessageInthread(service,userId,msgs);
+	    System.out.println("msg -- : " + msg);
+	    MimeMessage mim = getMimeMessage(service, userId, msg.getId());
+	    System.out.println("Mime message -- : "+getText(mim));
 	    return msg;
     }
     
@@ -221,6 +237,46 @@ public class GmailIntegrator {
         return message;
     }    
     
+	public static String getText(Part p) throws MessagingException, IOException {
+		if (p.isMimeType("text/*")) {
+			String s = (String) p.getContent(); 
+			return s;
+		}
+
+		if (p.isMimeType("multipart/alternative")) {
+			// prefer html text over plain text
+			Multipart mp = (Multipart) p.getContent();
+			String text = null;
+			for (int i = 0; i < mp.getCount(); i++) {
+				Part bp = mp.getBodyPart(i);
+				if (bp.isMimeType("text/plain")) {
+					if (text == null) {
+						text = getText(bp);
+					}
+					continue;
+				} else if (bp.isMimeType("text/html")) {
+					String s = getText(bp);
+					if (s != null) {
+						return s;
+					}
+				} else {
+					return getText(bp);
+				}
+			}
+			return text;
+		} else if (p.isMimeType("multipart/*")) {
+			Multipart mp = (Multipart) p.getContent();
+			for (int i = 0; i < mp.getCount(); i++) {
+				String s = getText(mp.getBodyPart(i));
+				if (s != null) {
+					return s;
+				}
+			}
+		}
+
+		return null;
+	}    
+    
     public static Message sendMessage(Gmail service, String userId, MimeMessage emailContent, String threadId) throws MessagingException, IOException, GeneralSecurityException {
 		Message message = createMessageWithEmail(emailContent, threadId);
 		message = service.users().messages().send(userId, message).execute();
@@ -230,7 +286,9 @@ public class GmailIntegrator {
     public static void main(String... args) throws IOException, GeneralSecurityException, AddressException, MessagingException {
         //Gmail service = getService();
         //String user = "me";
-        String subject = "Hello Hi bol ke";
-    	replyEmail(subject,"Tere aage pichhe dol ke :P ");
+        //String subject = "Please sign TSMC-00018433 Acknowledgement Letter";
+        System.out.println("*&*&* - "+getEchoSignLink("TSMC-00018433"));
+        //listMessagesMatchingQuery(service, user, subject);
+    	//replyEmail(subject,"Tere aage pichhe dol ke :P ");
     }
 }
